7b5ae60ef02ca35aec7fdd16d87f43ecc2822633
MC Naveen <8493007+mcnaveen@users.noreply.github.com>
2023-12-18 15:20:14 +0530

    :bug: fix script 404

--- public/class-formbricks-public.php
+++ public/class-formbricks-public.php
@@ -96,7 +96,7 @@ class Formbricks_Public {
 		 * class.
 		 */
 
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/formbricks-public.js', array( 'jquery' ), $this->version, false );
+		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/formbricks.js', array( 'jquery' ), $this->version, false );
 
 	}
 
