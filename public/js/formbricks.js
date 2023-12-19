65d79c705a9bd486a8a5f24485164cb97e54743e
Naveen MC <8493007+mcnaveen@users.noreply.github.com>
2023-12-20 00:29:18 +0530

    fix(js): :bug: fix debug flag
    This commit upates the branding logo and loads the JS only if the value is configured

--- public/js/formbricks.js
+++ public/js/formbricks.js
@@ -1,18 +1,20 @@
-(function () {
-  var t = document.createElement("script");
-  t.type = "text/javascript";
-  t.async = true;
-  t.src = "https://unpkg.com/@formbricks/js@latest/dist/index.umd.js";
-  var e = document.getElementsByTagName("script")[0];
-  e.parentNode.insertBefore(t, e);
+document.addEventListener("DOMContentLoaded", function () {
+  if (formbricksPluginSettings && formbricksPluginSettings.environmentId && formbricksPluginSettings.apiHost) {
+    var t = document.createElement("script");
+    t.type = "text/javascript";
+    t.async = true;
+    t.src = "https://unpkg.com/@formbricks/js@latest/dist/index.umd.js";
+    var e = document.getElementsByTagName("script")[0];
+    e.parentNode.insertBefore(t, e);
 
-  window.addEventListener("load", function () {
-    setTimeout(function () {
-      window.formbricks.init({
-        environmentId: formbricksPluginSettings.environmentId,
-        apiHost: formbricksPluginSettings.apiHost,
-        debug: formbricksPluginSettings.debug,
-      });
-    }, 500);
-  });
-})();
+    window.addEventListener("load", function () {
+      setTimeout(function () {
+        window.formbricks.init({
+          environmentId: formbricksPluginSettings.environmentId,
+          apiHost: formbricksPluginSettings.apiHost,
+          debug: Boolean(formbricksPluginSettings.debug),
+        });
+      }, 500);
+    });
+  }
+});
