1b09c880a280d436121e3a76a3a3b5598c01aeb2
ShubhamPalriwala <spalriwalau@gmail.com>
2024-03-02 18:14:07 +0530

    fix: sophisticated approach ugh

--- public/class-formbricks-public.php
+++ public/class-formbricks-public.php
@@ -103,8 +103,8 @@ class Formbricks_Public
 			$api_host = get_option('formbricks_api_host');
 
 			if (!empty($environment_id) && !empty($api_host)) {
-				wp_enqueue_script($this->plugin_name, plugin_dir_url(__FILE__) . 'js/formbricks.js', array('jquery'), $this->version, false);
 				wp_enqueue_script($this->plugin_name, plugin_dir_url(__FILE__) . 'js/formbricks-client.umd.js', array('jquery'), $this->version, false);
+				wp_enqueue_script($this->plugin_name, plugin_dir_url(__FILE__) . 'js/formbricks.js', array('jquery'), $this->version, false);
 			}
 		}
 	}
